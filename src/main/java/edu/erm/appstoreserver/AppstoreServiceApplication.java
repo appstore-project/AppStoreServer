266e4be70e9db6a02ac596e33232a1c5754ddfdd
erfan <debugsg>
2019-05-24 10:16:45 +0430

    Prevent "Access-Control-Allow-Origin" error On client (REACT) in server side.

--- src/main/java/edu/erm/appstoreserver/AppstoreServiceApplication.java
+++ src/main/java/edu/erm/appstoreserver/AppstoreServiceApplication.java
@@ -2,6 +2,10 @@ package edu.erm.appstoreserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class AppstoreServiceApplication {
@@ -10,4 +14,15 @@ public class AppstoreServiceApplication {
         SpringApplication.run(AppstoreServiceApplication.class, args);
     }
 
+    // Erfan: Enable Cross-Origin Request Sharing
+    // Erfan:  It not suitable for public API. must be more closer.
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
